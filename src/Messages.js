8eca28274292d75aba09c993e456a70b101f12c6
Anat Snir <anats2@gmail.com>
2019-10-24 17:32:50 +0300

    Added new message modal UI

--- src/Messages.js
+++ src/Messages.js
@@ -3,9 +3,10 @@ import React from 'react';
 //import Navbar from 'react-bootstrap/Navbar'
 //import Jumbotron from 'react-bootstrap/Jumbotron'
 //import Button from 'react-bootstrap/Button'
-import Container from 'react-bootstrap/Container'
 import MessagesNavbar from './components/MessagesNavbar'
 import MessagesAccordion from './components/MessagesAccordion'
+import { Container, Row, Col, Button, Modal, Form, Image } from 'react-bootstrap'
+
 
 //import Modal from 'react-bootstrap/Modal'
 //import Form from 'react-bootstrap/Form'
@@ -36,65 +37,76 @@ import MessagesAccordion from './components/MessagesAccordion'
 
 function Messages() {
   return (
-    <Container fluid className="Messages p-4">
-      <MessagesNavbar />
-      <div className="text-right pr-3 pt-4 pb-1">
-        <a href="#" style={{textDecoration:"underline", fontWeight:"bolder"}}>New Message</a>
-      </div>
-      <MessagesAccordion />
+      <div>
+        <Container fluid className="Messages p-4">
+            <MessagesNavbar />
+            <div className="text-right pr-3 pt-4 pb-1">
+                <a href="#" style={{textDecoration:"underline", fontWeight:"bolder"}}>New Message</a>
+            </div>
+            <MessagesAccordion />
+        </Container>
+        <Modal show="true" size="lg">
+            <Modal.Header closeButton>
+                <Modal.Title>New Message</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+                <Form className="messageModalFrm">
+                    <Form.Group as={Row} controlId="formNameTxt">
+                        <Form.Label column sm={2}>
+                            Title:
+                        </Form.Label>
+                        <Col sm={10}>
+                            <Form.Control type="text" placeholder="Message title" />
+                        </Col>
+                    </Form.Group>
 
-      {/* <Container>
-          <div className="recipes-header">
-              <h1>Messages</h1>
-          </div>
-      </Container>
+                    <Form.Group as={Row} controlId="formDetailsTxt">
+                        <Form.Label column sm={2}>
+                            Details:
+                        </Form.Label>
+                        <Col sm={10}>
+                            <Form.Control type="text" placeholder="Message Details" />
+                        </Col>
+                    </Form.Group>
 
+                    <Form.Group as={Row} controlId="formPrioritySelect">
+                        <Form.Label column sm={2}>
+                            Priority:
+                        </Form.Label>
+                        <Col sm={10}>
+                            <Form.Control as="select" className="priority-select">
+                                <option value="info">Information</option>
+                                <option value="important">Important</option>
+                            </Form.Control>
+                        </Col>
+                    </Form.Group>
 
-      <Modal onHide={this.closeModal} size="lg">
-          <Modal.Header closeButton>
-              <Modal.Title>New Recipe</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-              <Form>
-                  <Form.Group as={Row} controlId="formHorizontalEmail">
-                      <Form.Label column sm={2}>
-                          Name
-                      </Form.Label>
-                      <Col sm={10}>
-                          <Form.Control ref={this.nameInput} type="text" placeholder="Recipe name" />
-                      </Col>
-                  </Form.Group>
-
-                  <Form.Group as={Row} controlId="formHorizontalPassword">
-                      <Form.Label column sm={2}>
-                          Description
-                      </Form.Label>
-                      <Col sm={10}>
-                          <Form.Control ref={this.descInput} type="text" placeholder="Recipe description" />
-                      </Col>
-                  </Form.Group>
-
-                  <Form.Group as={Row} controlId="formHorizontalPassword">
-                      <Form.Label column sm={2}>
-                          Image
-                      </Form.Label>
-                      <Col sm={6}>
-                          <Form.Control type="file" placeholder="Recipe image URL" accept="image/*" onChange={this.imgChange}/>
-                      </Col>
-                  </Form.Group>
-
-              </Form>
-          </Modal.Body>
-          <Modal.Footer>
-              <Button variant="secondary" onClick={this.closeModal}>
-                  Close
-              </Button>
-              <Button variant="primary" onClick={this.createRecipe}>
-                  Create Recipe
-              </Button>
-          </Modal.Footer>
-      </Modal> */}
-    </Container>
+                    <Form.Group as={Row} controlId="formImgControl" className="align-items-center">
+                        <Form.Label column sm={2}>
+                            Image URL:
+                        </Form.Label>
+                        <Col sm={7}>
+                            <div class="custom-file">
+                                <input type="file" class="custom-file-input" id="customFile" />
+                                <label class="custom-file-label" for="customFile">Choose image</label>
+                            </div>
+                        </Col>
+                        <Col sm={3}>
+                            <Image src="https://interactive-examples.mdn.mozilla.net/media/examples/grapefruit-slice-332-332.jpg" fluid/>
+                        </Col>
+                    </Form.Group>
+                </Form>
+            </Modal.Body>
+            <Modal.Footer>
+                <Button variant="secondary">
+                    Close
+                </Button>
+                <Button variant="primary">
+                    Create
+                </Button>
+            </Modal.Footer>
+        </Modal>
+    </div>
   );
 }
 
