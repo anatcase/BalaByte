cff4e9e221cdc7427860763d826c58ac79c28c3a
Anat Snir <anats2@gmail.com>
2019-10-26 00:05:32 +0300

    npm installation of react-minimal-pie-chart

--- src/components/VotingsAccordion.js
+++ src/components/VotingsAccordion.js
@@ -53,8 +53,7 @@ class VotingsAccordion extends React.Component {
                                                         { title: 'Two', value: 15, color: '#C13C37' },
                                                         { title: 'Three', value: 20, color: '#6A2135' },
                                                     ]}
-                                                    />;
-                                                {/* <canvas id="pieChart"></canvas> */}
+                                                    />;                                               
                                             </Col>
                                         </Row>
                                     </Col>                                 
