cc9e56b89c0321772138c51bc1424276a97c5088
Anat Snir <anats2@gmail.com>
2019-10-26 16:39:51 +0300

    installed react-js-pagination

--- src/components/PaginationBar.js
+++ src/components/PaginationBar.js
@@ -1,59 +1,47 @@
 import React from 'react'
 import Pagination from 'react-bootstrap/Pagination'
 
-class PaginationBar extends React.Component {
-    constructor(props) {
-        super(props);
-    } 
+function Navigation() {
+  return (
+    <Pagination
+        activePage={this.state.activePage}
+        itemsCountPerPage={PER_PAGE}
+        totalItemsCount={TOTAL_COUNT}
+        onChange={this.handlePageChange}
+    />
+  );
+}
 
-    render() {
-        let active = 2; //this will be the valu of the event.target eg the page number button clicked on the pagination comp
-        let pages = 10; //this will be the number of voting records divided by the number of record we would like to display on the page
-        let items = [];
-        for (let number = 1; number <= pages; number++) {
-        items.push(
-            <Pagination.Item key={number} active={number === active}>
-                {number}
-            </Pagination.Item>,
-        );
-    }
+// class PaginationBar extends React.Component {
 
-    const paginationBasic = (
-          <Pagination size="sm">
-                <Pagination.First />
-                <Pagination.Prev /> 
-                {items}
-                <Pagination.Next />
-                <Pagination.Last />
-          </Pagination>
-      );
+//     render() {
+//         let active = 2; //this will be the valu of the event.target eg the page number button clicked on the pagination comp
+//         let pages = 10; //this will be the number of voting records divided by the number of record we would like to display on the page
+//         let items = [];
+//         for (let number = 1; number <= pages; number++) {
+//         items.push(
+//             <Pagination.Item key={number} active={number === active}>
+//                 {number}
+//             </Pagination.Item>,
+//         );
+//     }
+
+//     const paginationBasic = (
+//           <Pagination size="sm">
+//                 <Pagination.First />
+//                 <Pagination.Prev /> 
+//                 {items}
+//                 <Pagination.Next />
+//                 <Pagination.Last />
+//           </Pagination>
+//       );
         
-      return (
-            <div className="Pagination">
-                {paginationBasic}    
-            </div>
-        
-                // <div className="Pagination">
-                //     <Pagination>
-                //     <Pagination.First />
-                //     <Pagination.Prev />
-                //     <Pagination.Item>{1}</Pagination.Item>
-                //     <Pagination.Ellipsis />
-
-                //     <Pagination.Item>{10}</Pagination.Item>
-                //     <Pagination.Item>{11}</Pagination.Item>
-                //     <Pagination.Item active>{12}</Pagination.Item>
-                //     <Pagination.Item>{13}</Pagination.Item>
-                //     <Pagination.Item disabled>{14}</Pagination.Item>
-
-                //     <Pagination.Ellipsis />
-                //     <Pagination.Item>{20}</Pagination.Item>
-                //     <Pagination.Next />
-                //     <Pagination.Last />
-                //     </Pagination>
-                // </div>
-        );
-    }
-  }
+//       return (
+//             <div className="Pagination">
+//                 {paginationBasic}    
+//             </div>
+//         );
+//     }
+//   }
 
   export default PaginationBar;
