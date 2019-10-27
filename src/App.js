d8b2535d0f8a511862cc1b6f0f549d0ed59e91a3
Anat Snir <anats2@gmail.com>
2019-10-27 13:29:08 +0200

    removed messages old.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 //import Home from './Home';
 //import SignUp from './SignUp';
 //import Login from './Login';
-//import Messages from './Messages';
+import Messages from './Messages';
 //import Tenants from './Tenants';
 //import Votings from './Votings';
-import PaginationNav from './components/PaginationNav';
+//import PaginationNav from './components/PaginationNav';
 
 class App extends React.Component {
   constructor(props) {
@@ -21,22 +21,38 @@ class App extends React.Component {
   }
   
   handlePageChange(e) {
-    var pageNumber =  parseInt(e.target.innerHTML);
-    if (isNaN(pageNumber)) {
-      console.log('Not a number' + pageNumber);
-    }
+    let val = parseInt(e.target.innerHTML);
+    let pageNumber;
+
+    if (isNaN(val)) {
+      console.log('Not a number ' + val);
+      val = e.target.innerText;
+      if (val.includes("‹")) {
+        console.log("Previous");
+      }
+      else if (val.includes("›")) {
+        console.log("Next");
+      }
       
+    }
+    else {
+      console.log('number ' + val);
+      pageNumber =  val;
+    }
     console.log('active page is ' + pageNumber);
-    this.state.activePage = pageNumber;
+    //this.state.activePage = pageNumber;
     console.log(this.state.activePage);
-    this.setState(this.state);
+    //this.setState(this.state);
+    this.setState({activePage:pageNumber});
+    console.log(this.state.activePage);
   }
 
   render() {
     console.log(this.state.activePage);
     return (
       <div className="App">
-         <PaginationNav handlePageChange={this.handlePageChange} activePage={this.state.activePage}/>
+         {/* <PaginationNav handlePageChange={this.handlePageChange} activePage={this.state.activePage}/> */}
+         <Messages/>
       </div>
     );
   }
