5fb3a602542ba3b22ce256759edddf002651d164
Anat Snir <anats2@gmail.com>
2019-10-30 06:11:12 +0200

    Updates

---
+++ precache-manifest.5b27b27c963774c21764ea9b9156c118.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e6e4f56159ea981c2e6eb2055c3b8582",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e4a2045792c9ea697b1",
+    "url": "/static/css/2.4e0883bb.chunk.css"
+  },
+  {
+    "revision": "2b8009edafc39e8ec12b",
+    "url": "/static/css/main.b754ce78.chunk.css"
+  },
+  {
+    "revision": "7e4a2045792c9ea697b1",
+    "url": "/static/js/2.be96e77d.chunk.js"
+  },
+  {
+    "revision": "2b8009edafc39e8ec12b",
+    "url": "/static/js/main.fd2fbf7d.chunk.js"
+  },
+  {
+    "revision": "002a369578e1fc7be1c1",
+    "url": "/static/js/runtime-main.ef27e905.js"
+  },
+  {
+    "revision": "8bad87734ac6c834243fa4eb96cc1fe1",
+    "url": "/static/media/angle-down-solid.8bad8773.svg"
+  }
+]);
