bee21acaed5c6f83d6333da616ae7edf075dda85
Anat Snir <anats2@gmail.com>
2019-10-31 02:49:18 +0200

    Updates

---
+++ precache-manifest.8fac4f57e94270a5c8d631ca4044fc47.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86db9f0149c9d91d3fe1f948e3215be2",
+    "url": "/homeboy/index.html"
+  },
+  {
+    "revision": "8855bfe88aa5cb1f8b4c",
+    "url": "/homeboy/static/css/2.4e0883bb.chunk.css"
+  },
+  {
+    "revision": "87dedeeaa0a31cae7ca1",
+    "url": "/homeboy/static/css/main.568cd429.chunk.css"
+  },
+  {
+    "revision": "8855bfe88aa5cb1f8b4c",
+    "url": "/homeboy/static/js/2.b7c1c1f2.chunk.js"
+  },
+  {
+    "revision": "87dedeeaa0a31cae7ca1",
+    "url": "/homeboy/static/js/main.47eb9971.chunk.js"
+  },
+  {
+    "revision": "82477de94d4184d17e50",
+    "url": "/homeboy/static/js/runtime-main.73350fa6.js"
+  },
+  {
+    "revision": "6060a92ea1f15d726432d5a9bbc6630f",
+    "url": "/homeboy/static/media/angle-down-solid.6060a92e.svg"
+  }
+]);
