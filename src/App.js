fbfe979ff0b983a03bacc033fcf4e2e42ed5f4f2
Anat Snir <anats2@gmail.com>
2019-11-01 16:46:22 +0200

    https://anatcase.github.io/homeboy/

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ class App extends React.Component {
     super(props);
     this.state = {
       activeUser: null,
-      isLoggedIn: true,
+      isLoggedIn: false,
       // allUsers: jsonUsers,
   }
 
