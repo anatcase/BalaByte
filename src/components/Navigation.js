e7425a5f16f950d16405567e5131d550ce04d60d
Anat Snir <anats2@gmail.com>
2019-11-01 16:07:22 +0200

    changed link for logo

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -14,7 +14,7 @@ class Navigation extends React.Component {
             <Navbar fixed="top" collapseOnSelect expand="lg">
               <Row className="w-100">
                     <Col lg={1} sm= {4} xs={4}>
-                      <Navbar.Brand href="Home"><img className={logo} src="./images/homeboy.png" alt="Homeboy - Your Buddy In Da Building" /></Navbar.Brand>
+                      <Navbar.Brand href="/"><img className={logo} src="./images/homeboy.png" alt="Homeboy - Your Buddy In Da Building" /></Navbar.Brand>
                     </Col>
                    
                   <Col lg={11} sm= {4} xs={4} className="px-0">
