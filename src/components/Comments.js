e82d0f1c7b2d4be1e29dee167e1282d612a338c8
Anat Snir <anats2@gmail.com>
2019-11-23 19:36:05 +0200

    comment username and image logic for tenant change

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -4,6 +4,8 @@ import Col from 'react-bootstrap/Col'
 import Form from 'react-bootstrap/Form'
 import CommentDB from '../components/CommentDB';
 import ImageHandler from '../components/ImageHandler';
+import UserDB from '../components/UserDB';
+
 
 
 class Comments extends React.Component {
@@ -12,9 +14,11 @@ class Comments extends React.Component {
 
         this.state = {
             comments : null,
-            parentId : this.props.parentId
+            parentId : this.props.parentId,
+            users: null
         }
 
+        this.getUserName = this.getUserName.bind(this);
         this.getUserImage = this.getUserImage.bind(this);
         this.getCommentRows = this.getCommentRows.bind(this);
         this.handleKeyPress = this.handleKeyPress.bind(this);
@@ -23,16 +27,36 @@ class Comments extends React.Component {
         this.onAddCommentSuccess = this.onAddCommentSuccess.bind(this);
         this.onAddCommentError = this.onAddCommentError.bind(this);
 
+        this.onGetAllUsersSuccess = this.onGetAllUsersSuccess.bind(this);
+        this.onGetAllUsersError = this.onGetAllUsersError.bind(this);
+        
         CommentDB.GetObjectComments(this.state.parentId, this.onGetAllCommentsSuccess, this.onGetAllCommentsError)
 
         this.commentInput = React.createRef();
 
+
     }
+
+    onGetAllUsersSuccess(users) {
+        var userMap = new Map();
+
+        users.map((user) => userMap.set(user.id, user));
+
+        this.setState({users: userMap});
+
+
+    }
+
+    onGetAllUsersError(error) {
+    }
+
 
     componentDidMount(){
         // console.log("Getting All Comments for " + this.state.parentId);
        // CommentDB.GetObjectComments(this.state.parentId, this.onGetAllCommentsSuccess, this.onGetAllCommentsError)
         //this.props.getAllComments(this.state.parentId, this.onGetAllCommentsSuccess, this.onGetAllCommentsError)
+        UserDB.GetAllUsers(this.onGetAllUsersSuccess, this.onGetAllUsersError);
+
     }
 
     onGetAllCommentsSuccess(comments) {
@@ -68,9 +92,31 @@ class Comments extends React.Component {
           }
     }
 
+    getUserName(comment){
+        const commentUserId = comment.get('createdBy').id;
+        const commentUserName = comment.get('createdByUserName');
+        const user = this.state.users.get(commentUserId);
+        var userName = null;
+        if (user !== null && user !== undefined) {
+            userName = user.get('username');
+        }
+        if(userName == null) {
+            return commentUserName;
+        }
+        else {
+            return userName;
+        }
+    }
+
     getUserImage(comment){
-        const image = comment.get('createdByUserImage');
-        if(image == null) {
+
+        const user = this.state.users.get(comment.get('createdBy').id);
+        var image = null;
+        if (user !== null && user !== undefined) {
+            image = user.get('userImage');
+        }
+            //const image = null;//user.get('userImage');//comment.get('createdByUserImage');
+        if(image == null || image == undefined || image == "") {
             return "./images/avatar-placeholder.gif";
         }
         else {
@@ -89,7 +135,7 @@ class Comments extends React.Component {
                         <img className="rounded-circle avatar" src={this.getUserImage(comment)} alt="Avatar"></img>
                     </Col>
                     <Col lg={10} sm={10} xs={10} className="pl-0">
-                        <p className="font-weight-bold m-0">{comment.get('createdByUserName')}</p>
+                        <p className="font-weight-bold m-0">{this.getUserName(comment)}</p>
                         <p>{comment.get('text')}</p>
                     </Col>
                 </Row>
@@ -99,6 +145,9 @@ class Comments extends React.Component {
     }
 
     render () {
+        if (this.state.users === null) {
+            return <p>Loading</p>;
+        }
         const comments = this.state.comments;
         
 
